dbd1ee25d2176b638fa8ac08e5a2f4c02109b69e
= <jared@upstatement.com>
2015-12-23 21:51:17 -0500

    Basic new landing page

--- page-news.php
+++ page-news.php
@@ -1,2 +1,6 @@
 <?php
-	echo 'hello';
+	$context = Timber::get_context();
+	$posts = Timber::get_posts(array('post_type' => 'post'));
+	$context['menu'] = new TimberMenu();
+ 	$context['posts'] = $posts;
+	Timber::render('index.twig', $context);
